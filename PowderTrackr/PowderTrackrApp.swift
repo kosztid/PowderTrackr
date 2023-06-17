ac0c337f78c26e6ec8d0748dc5286808623bdb6d
Kosztolánczi Dominik <kosztolanczi.dominik@autsoft.hu>
2023-06-17 16:21:38 +0200

    Initial Commit

---
+++ PowderTrackr/PowderTrackrApp.swift
@@ -0,0 +1,17 @@
+//
+//  PowderTrackrApp.swift
+//  PowderTrackr
+//
+//  Created by Kosztolánczi Dominik on 2023. 06. 17..
+//
+
+import SwiftUI
+
+@main
+struct PowderTrackrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
