47dcf55331654d7e8117f6b603e01bb19148ad13
Kosztolánczi Dominik <kosztolanczi.dominik@autsoft.hu>
2023-08-05 22:07:25 +0200

    Update chat bar

--- PowderTrackr/Scenes/Social/ChatView/PowderTrackrChatView.swift
+++ PowderTrackr/Scenes/Social/ChatView/PowderTrackrChatView.swift
@@ -7,6 +7,48 @@ struct PowderTrackrChatView: View {
     var body: some View {
         ChatView(messages: viewModel.messages) { message in
             viewModel.sendMessage(draftMessage: message)
+        } inputViewBuilder: { textBinding, attachments, state, style, actionClosure in
+            Group {
+                switch style {
+                case .message:
+                    HStack {
+                        TextField("Write your message", text: textBinding)
+                        HStack {
+                            Button {
+                                actionClosure(.photo)
+                            } label: {
+                                Image(systemName: "photo.on.rectangle")
+                                    .frame(width: 48, height: 48)
+                                    .foregroundColor(.gray)
+                            }
+                            Button {
+                                actionClosure(.send)
+                            } label: {
+                                Image(systemName: "paperplane")
+                                    .frame(width: 48, height: 48)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+                    .padding(.leading, 16)
+                    .background(.white)
+                    .cornerRadius(16, corners: [.topLeft, .topRight])
+                    .shadow(
+                        color: .gray.opacity(0.2),
+                        radius: 4,
+                        x: 0,
+                        y: -4
+                    )
+                case .signature:
+                    VStack {
+                        HStack {
+                            Button("Send") { actionClosure(.send) }
+                        }
+                        TextField("Compose a signature for photo", text: textBinding)
+                            .background(Color.green)
+                    }
+                }
+            }
         }
         .mediaPickerTheme(
             main: .init(
