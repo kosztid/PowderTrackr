5b09d620b228ab53cfe561aef11fae88a7d10df6
Dominik Kosztolánczi <koszti.dominik@gmail.com>
2024-04-22 12:17:37 +0200

    add new cornerradius

--- PowderTrackr/Extensions/View+Extension/CornerRadius.swift
+++ PowderTrackr/Extensions/View+Extension/CornerRadius.swift
@@ -1,8 +1,27 @@
-//
-//  CornerRadius.swift
-//  PowderTrackr
-//
-//  Created by Dominik Kosztolánczi on 22/04/2024.
-//
+import SwiftUI
 
-import Foundation
+struct CornerRadiusStyle: ViewModifier {
+    var radius: CGFloat
+    var corners: UIRectCorner
+
+    func body(content: Content) -> some View {
+        content
+            .clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat
+    var corners: UIRectCorner
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        self.modifier(CornerRadiusStyle(radius: radius, corners: corners))
+    }
+}
